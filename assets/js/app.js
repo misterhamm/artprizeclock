962675220881bf8e65e266effb7e19e72b960ca8
Chris Hamm <chris.m.hamm@gmail.com>
2015-09-17 16:23:48 -0400

    logic reworked to display different countdown depending on whether it is before, during, or after an event.  Further styling, footer bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,26 +2,51 @@ var clock;
 			
 			$(document).ready(function() {
 				// Set dates.
-				var futureDate  = new Date("September 23, 2015 12:00 PM EDT");
+				var startDate   = new Date("September 23, 2015 12:00 PM EDT");
 				var currentDate = new Date();
+                var endDate     = new Date("October 15, 2015 10:00 pm EDT");
 
-				// Calculate the difference in seconds between the future and current date
-				var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+				var diff = 0;
+                
+				
+                //Display Counter Heading
+                if (currentDate < startDate) {
+                    $('.heading').html("Time until ArtPrize");
+                    // Calculate the difference in seconds between the future and current date
+                    var diff = startDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
-				// Calculate day difference and apply class to .clock for extra digit styling.
-				function dayDiff(first, second) {
-					return (second-first)/(1000*60*60*24);
-				}
+                    // Calculate day difference and apply class to .clock for extra digit styling.
+                    function dayDiff(first, second) {
+                        return (second-first)/(1000*60*60*24);
+                    }
 
-				if (dayDiff(currentDate, futureDate) < 100) {
-					$('.clock').addClass('twoDayDigits');
-				} else {
-					$('.clock').addClass('threeDayDigits');
-				}
+                    if (dayDiff(currentDate, startDate) < 100) {
+                        $('.clock').addClass('twoDayDigits');
+                    } else {
+                        $('.clock').addClass('threeDayDigits');
+                    }
+                    }
+                else if (currentDate > startDate && currentDate < endDate) {
+                    $('.heading').html("Time left to enjoy ArtPrize");
+                    $('.message').html('What are you doing here?? <br>Go see some art!!!');
+                    // Calculate the difference in seconds between the future and current date
+                    var diff = endDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
-				if(diff < 0) {
-					diff = 0;
-				}
+                    // Calculate day difference and apply class to .clock for extra digit styling.
+                    function dayDiff(first, second) {
+                        return (second-first)/(1000*60*60*24);
+                    }
+
+                    if (dayDiff(currentDate, endDate) < 100) {
+                        $('.clock').addClass('twoDayDigits');
+                    } else {
+                        $('.clock').addClass('threeDayDigits');
+                    }
+                }
+                
+                else {
+                    $('.message').html("Check back soon for a new countdown!");
+                }
 
 				// Instantiate a coutdown FlipClock
 				clock = $('.clock').FlipClock(diff, {
